fb14b0b71edfdad04e5b14331ce52b35042b03f7
togrulagayev <togrullagayev@gmail.com>
2024-03-26 17:03:30 +0400

    update

--- Dart Learning/week_two/final_const.dart
+++ Dart Learning/week_two/final_const.dart
@@ -3,18 +3,18 @@ void main() {
 
   String name2 = 'Polad';
   name2 = 'Ramin';
-  // print(name2);
-  const String name = 'Suat';
+  print(name2);
+  // const String name = 'Suat';
   // name = 'Cavid'; // ! Error`
 
-  int age2;
-  age2 = 30;
-  const int age = 20;
+  // int age2;
+  // age2 = 30;
+  // const int age = 20;
 
-  final surname = 'Mammadov';
+  // final surname = 'Mammadov';
   // surname = 'Aliyev'; // ! Error
 
-  final username2;
-  username2 = 'Babazade';
+  // final username2;
+  // username2 = 'Babazade';
   // username2 = 4;
 }
---
+++ Dart Learning/week_two/where.dart
@@ -0,0 +1,15 @@
+void main() {
+  // List<int> numbers = [1, 3, 10, 7, 2, 9, 5, 4, 6];
+
+  // var result = numbers.any((element) {
+  //   if (element.isEven) {
+  //     print("$element is even");
+  //   } else {
+  //     print("$element is odd");
+  //   }
+  //   return false;
+  // });
+
+  var xz = true;
+  print(!xz);
+}
